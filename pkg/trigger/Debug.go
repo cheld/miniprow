24d7fc614cf54bc69ed7187831aeedcb8467ec25
Christoph Held <christoph.held@nttdata.com>
2020-11-05 09:21:59 +0100

    clean up config

--- pkg/trigger/Debug.go
+++ pkg/trigger/Debug.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Stdout(trigger config.Trigger, triggerInput config.TriggerInput) {
-	messageTemplate := trigger.Arguments["text"].(string)
+	messageTemplate := trigger.Arguments["stdout"].(string)
 	t, _ := config.ProcessTemplate(messageTemplate, triggerInput)
 	fmt.Println(t)
 }
