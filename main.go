968db710753d6fa217be31fab69b2cd373b905e7
Christoph Held <christoph.held@nttdata.com>
2020-11-06 08:19:57 +0100

    added webhook package

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/cheld/cicd-bot/pkg/config"
 	"github.com/cheld/cicd-bot/pkg/event"
 	"github.com/cheld/cicd-bot/pkg/trigger"
-	"gopkg.in/go-playground/webhooks.v5/github"
-)
-
-const (
-	pathGithubWebhook = "/webhooks/github"
+	"github.com/cheld/cicd-bot/pkg/webhook"
 )
 
 func main() {
@@ -19,25 +12,8 @@ func main() {
 	config := config.Load("config.yaml")
 	args := "test"
 	stdin := ""
-	handler := event.NewHandler(config)
-	dispatcher := trigger.NewDispatcher(config)
-	triggerInput := handler.HandleCli(args, stdin)
-	fmt.Println(triggerInput)
+	triggerInput := event.NewHandler(config).HandleCli(args, stdin)
 	trigger.NewDispatcher(config).Execute(triggerInput)
 
-	//event := source.NewGithub(config).ParseInput(payload)
-	// destination.New(config).Execute(event)
-
-	githubWebhook, _ := github.New(github.Options.Secret("MySecret"))
-
-	http.HandleFunc(pathGithubWebhook, func(w http.ResponseWriter, r *http.Request) {
-		payload, err := githubWebhook.Parse(r, github.IssueCommentEvent)
-		if err != nil {
-			if err == github.ErrEventNotFound {
-				// ok event wasn;t one of the ones asked to be parsed
-			}
-		}
-		dispatcher.Execute(handler.HandleGithub(payload))
-	})
-	http.ListenAndServe(":3000", nil)
+	webhook.Run()
 }
---
+++ pkg/webhook/server.go
@@ -0,0 +1,34 @@
+package webhook
+
+import (
+	"net/http"
+
+	"github.com/cheld/cicd-bot/pkg/config"
+	"github.com/cheld/cicd-bot/pkg/event"
+	"github.com/cheld/cicd-bot/pkg/trigger"
+	"gopkg.in/go-playground/webhooks.v5/github"
+)
+
+const (
+	pathGithubWebhook = "/webhooks/github"
+)
+
+func Run() {
+	config := config.Load("config.yaml")
+
+	handler := event.NewHandler(config)
+	dispatcher := trigger.NewDispatcher(config)
+
+	githubWebhook, _ := github.New(github.Options.Secret("MySecret"))
+
+	http.HandleFunc(pathGithubWebhook, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := githubWebhook.Parse(r, github.IssueCommentEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				// ok event wasn;t one of the ones asked to be parsed
+			}
+		}
+		dispatcher.Execute(handler.HandleGithub(payload))
+	})
+	http.ListenAndServe(":3000", nil)
+}
